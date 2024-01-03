27a35dd2d25882b6c95f26e52749a499f0ddbf05
Christian Ramos <ramosbarcena12@gmail.com>
2024-01-03 00:20:41 -0500

    First Commit

---
+++ src/js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Estudiante de Sistemas!" , "Hacker Ético!", "Desarrollador Web!" , "AWS Cloud Practitioner!"], 
+        typeSpeed: 110,
+        backSpeed: 55,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Estudiante de Sistemas" , "Hacker Ético!", "Desarrollador Web!" , "AWS Cloud Practitioner!"],
+        typeSpeed: 110,
+        backSpeed: 55,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            850:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 2,
+                nav: false
+            }
+        }
+    }); 
+}); 
